a7573bc9c13069a573837d4513a31bb8c96d2604
vidyasagar reddy <vidyasagar@vidyasagars-MacBook-Pro.local>
2025-08-14 21:52:37 -0500

    Initial commit

---
+++ src/com/solving/TwoSum.java
@@ -0,0 +1,30 @@
+package com.solving;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int[] nums = {2,7,11,15};
+		
+		int target = 9;
+
+	}
+	
+	public int[] twoSum(int[] nums, int target) {
+		
+		Map<Integer,Integer> targetMap = new  HashMap<>();
+		 
+		for(int i =0 ; i < nums.length; i++) {
+			
+			
+			
+		}
+		
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module BLIND75 {
+}
